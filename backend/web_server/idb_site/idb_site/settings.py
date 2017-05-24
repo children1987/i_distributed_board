384b633d12a73e0f52c76f56197dd04d9df24047
children1987 <39188043@qq.com>
2017-05-25 00:24:35 +0800

    忽略一种文件的版本记录；允许所有主机访问

--- backend/web_server/idb_site/idb_site/settings.py
+++ backend/web_server/idb_site/idb_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uo^xo(_w7qftpli)-7i3f%l-z^_e2wa9jr=*&gr3n^g89l)%oh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
